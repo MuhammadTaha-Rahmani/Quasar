c43bf6901a8cd5948b2c618ebdc9da8c37bf0df0
M.T Rahmani <mohammadtaharahmani500@gmail.com>
2024-01-26 21:02:09 +0330

    add image to signUp page

--- quasar/src/pages/signUp.vue
+++ quasar/src/pages/signUp.vue
@@ -1,31 +1,38 @@
 <template>
-  <q-page class="flex flex-center  q-py-xl">
-    <div class="glass column q-gutter-y-lg q-pa-xl">
-      <h3 class="q-mt-none q-mb-lg text-center text-primary">عضویت</h3>
-      <p class="text-center q-my-none">
-        قبلا ثبت نام کزده اید ؟ <span class="text-primary">ورود</span>
-      </p>
-      <q-input standout="bg-primary text-white" label="نام">
-        <template v-slot:prepend>
-          <q-icon name="person" />
-        </template>
-      </q-input>
-      <q-input standout="bg-primary text-white" label="شماره تلفن">
-        <template v-slot:prepend>
-          <q-icon name="phone" />
-        </template>
-      </q-input>
-      <q-input standout="bg-primary text-white" label="ایمیل">
-        <template v-slot:prepend>
-          <q-icon name="mail" />
-        </template>
-      </q-input>
-      <q-input standout="bg-primary text-white" label="رمز عبور">
-        <template v-slot:prepend>
-          <q-icon name="password" />
-        </template>
-      </q-input>
-      <q-btn class="bg-primary text-white" label="ادامه" />
+  <q-page>
+    <div class="row items-center justify-evenly q-my-xl">
+      <div class="col-5">
+        <div class="glass column q-gutter-y-lg q-pa-xl">
+          <h3 class="q-mt-none q-mb-lg text-center text-primary">عضویت</h3>
+          <p class="text-center q-my-none">
+            قبلا ثبت نام کزده اید ؟ <span class="text-primary">ورود</span>
+          </p>
+          <q-input standout="bg-primary text-white" label="نام">
+            <template v-slot:prepend>
+              <q-icon name="person" />
+            </template>
+          </q-input>
+          <q-input standout="bg-primary text-white" label="شماره تلفن">
+            <template v-slot:prepend>
+              <q-icon name="phone" />
+            </template>
+          </q-input>
+          <q-input standout="bg-primary text-white" label="ایمیل">
+            <template v-slot:prepend>
+              <q-icon name="mail" />
+            </template>
+          </q-input>
+          <q-input standout="bg-primary text-white" label="رمز عبور">
+            <template v-slot:prepend>
+              <q-icon name="password" />
+            </template>
+          </q-input>
+          <q-btn class="bg-primary text-white" label="ادامه" />
+        </div>
+      </div>
+      <div class="col-11 col-md-5">
+        <q-img width="475px" style="display:block;" class="q-mx-auto q-my-none" src="../assets/signUp.png" />
+      </div>
     </div>
   </q-page>
 </template>
